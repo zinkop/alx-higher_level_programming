3bcb0db57b39ba774882d76b11675d90ba2f1542
Zinkop <agalabri.alex@gmail.com>
2022-07-06 02:57:51 -0700

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""read file"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
+    """read file"""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
